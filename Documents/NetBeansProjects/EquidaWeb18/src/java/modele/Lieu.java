7098dbc8ed9ebf3e591006cfffc00913d8cbc374
SimonL <simon.louet.98@gmail.com>
2018-09-18 14:09:04 +0200

    Ajout Classe Lieu

---
+++ Documents/NetBeansProjects/EquidaWeb18/src/java/modele/Lieu.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Simon Louet
+ */
+public class Lieu {
+    private int id;
+    private String ville ;
+    private int nbBoxes;
+    private String commentaire ;
+    
+    
+    public Lieu() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public int getNbBoxes() {
+        return nbBoxes;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public void setNbBoxes(int nbBoxes) {
+        this.nbBoxes = nbBoxes;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+    
+}
