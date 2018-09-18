6933d2d6d7c85b0543b8ff74773e62d9da88611b
SimonL <simon.louet.98@gmail.com>
2018-09-18 14:11:07 +0200

    Ajout Classe Courriel

---
+++ Documents/NetBeansProjects/EquidaWeb18/src/java/modele/Courriel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author slam
+ */
+public class Courriel {
+    private int id;
+    private String date ;
+    private String objet;
+    private String corps ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getObjet() {
+        return objet;
+    }
+
+    public void setObjet(String objet) {
+        this.objet = objet;
+    }
+
+    public String getCorps() {
+        return corps;
+    }
+
+    public void setCorps(String corps) {
+        this.corps = corps;
+    }
+    
+    
+}
